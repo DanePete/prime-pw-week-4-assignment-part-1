5d171ce4780a02c5be9bdea7639091857b9ce73d
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-03 11:08:38 -0500

    completed question 6

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -57,8 +57,13 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast( array ) {
-
+  let last = array[array.length - 1]
+  return last;
 }
+
+let favFoods = ['pasta', 'meat and potatoes', 'chinese', 'mexican', 'indian']
+
+console.log(getLast(favFoods));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
