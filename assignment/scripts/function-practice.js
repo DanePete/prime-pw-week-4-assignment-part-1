2f228f518df2a7f708051c51879e64d08762eaf5
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-03 11:01:05 -0500

    working on problems 1-4

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,22 +15,30 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return name;
 }
 // Remember to call the function to test
+console.log('Hello,', helloName('Dane') + '!');
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
   // return firstNumber + secondNumber;
+  return firstNumber + secondNumber;
 }
 
+console.log(addNumbers(2, 5));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
+function multiplyThree( num1, num2, num3 ){
 
+
+  let result = num1 * num2 * num3;
+
+  return result;
 }
 
+console.log(multiplyThree(1,2,3));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
